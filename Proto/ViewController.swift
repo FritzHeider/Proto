9bf79bcc8075d9a0ff401866d396db55883c2d49
FritzHeider <fritz.heider@students.makeschool.com>
2019-12-12 01:40:50 -0800

    prototype

---
+++ Proto/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Proto
+//
+//  Created by Fritz Heider on 12/10/19.
+//  Copyright © 2019 Make School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var signIn: UIButton!
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func signIn(_ sender: Any) {
+    }
+    
+
+}
+
